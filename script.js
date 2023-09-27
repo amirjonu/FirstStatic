0b3205b2133b66b6faca51d303afa3227a250fd5
Amirjon Ulmasov <amirjonu@nycstudents.net>
2023-09-26 20:03:51 -0400

    finished home page and added video

---
+++ script.js
@@ -0,0 +1,37 @@
+const bars= document.querySelector(".bars");
+const ham= document.querySelector(".hamburger");
+
+bars.addEventListener("click", () =>{
+    bars.classList.toggle("active");
+    ham.classList.toggle("active");
+});
+
+document.querySelectorAll(".pageslink").forEach(n=> n.addEventListener("click", ()=>{
+    bars.classList.remove("active");
+    ham.classList.remove("active");
+}));
+
+
+var pokeball = document.getElementById("pokeball");
+pokeball.addEventListener("click", () => {
+  pokeball.classList.add("pokeball-clicked");
+});
+
+
+const welcomeMessage= document.querySelector(".welcomeMessage");
+const toPokemon= document.querySelector(".toPokemon");
+
+
+function checkOpacity() {
+    const ist = getComputedStyle(pokeball);
+    const opacity = parseFloat(ist.opacity);
+
+    if (opacity === 0) {
+        welcomeMessage.classList.toggle("active");
+        toPokemon.classList.toggle("active");
+    }
+}
+
+pokeball.addEventListener("click", () => {
+    setTimeout(checkOpacity, 5500); 
+});
